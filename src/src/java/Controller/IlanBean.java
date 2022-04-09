3875680de8a4ccbbb6fb53fe341ccd1896a9a42b
a.faruk <->
2022-04-09 17:40:42 +0300

    IlanBean.java eklendi

---
+++ src/src/java/Controller/IlanBean.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package Controller;
+
+import DAO.IlanDAO;
+import Entity.Ilan;
+import jakarta.inject.Named;
+import jakarta.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import java.util.List;
+
+
+@Named(value = "ilanBean")
+@SessionScoped
+public class IlanBean implements Serializable {
+
+    private IlanDAO dao;
+    private Ilan entity;
+    private List<Ilan> list;
+
+    public IlanDAO getDao() {
+        if(this.dao == null) this.dao = new IlanDAO();
+        return dao;
+    }
+
+    public void setDao(IlanDAO dao) {
+        this.dao = dao;
+    }
+
+    public Ilan getEntity() {
+        if(this.entity == null) this.entity = new Ilan();
+        return entity;
+    }
+
+    public void setEntity(Ilan entity) {
+        this.entity = entity;
+    }
+
+    public List<Ilan> getList() {
+        this.list = this.dao.getList();
+        return list;
+    }
+
+    public void setList(List<Ilan> list) {
+        this.list = list;
+    }
+    
+    public String createEntity() {
+        this.getDao().create(this.entity);
+        this.entity = new Ilan();
+        return "/ilanlar.xhtml";
+    }
+    
+    public String deleteEntity() {
+        this.getDao().delete(entity);
+        this.entity = new Ilan();
+        return "/ilanlar.xhtml";
+    }
+}
