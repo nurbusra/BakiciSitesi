e6f2fb4d67ffca2dc3857db78b57ccfa0979b73d
mer <->
2022-04-10 17:33:19 +0300

    bakiciDao'ya update() methodu eklendi

--- src/src/java/DAO/BakiciDAO.java
+++ src/src/java/DAO/BakiciDAO.java
@@ -15,20 +15,20 @@ public class BakiciDAO extends DBConnection {
     public void create(Bakici c) {
         try {
             Statement st = this.getDb().createStatement();
-            String values = "VALUES(" + 
-                    "'" + c.getIsim() + "'" + ", " +
-                    "'" + c.getEmail() + "'" + ", " +
-                    "'" + c.getSifre() + "'" + ", " +
-                          c.getSinif() + 
-                    ");";
-            
-            String query = 
-                    "insert into BAKICI(isim, email, sifre, sinif) " + values;
-                            
+            String values = "VALUES("
+                    + "'" + c.getIsim() + "'" + ", "
+                    + "'" + c.getEmail() + "'" + ", "
+                    + "'" + c.getSifre() + "'" + ", "
+                    + c.getSinif()
+                    + ");";
+
+            String query
+                    = "insert into BAKICI(isim, email, sifre, sinif) " + values;
+
             int r = st.executeUpdate(query);
-            
+
             System.out.println("DB INSERT returned with: " + r);
-            
+
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
@@ -37,11 +37,30 @@ public class BakiciDAO extends DBConnection {
     public void delete(Bakici c) {
         try {
             Statement st = this.getDb().createStatement();
-            String query = "delete from BAKICI where bakici_id=" + c.getUser_id();          
+            String query = "delete from BAKICI where bakici_id=" + c.getUser_id();
             int r = st.executeUpdate(query);
-            
+
             System.out.println("DB DELETE returned with: " + r);
-            
+
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public void update(Bakici c) {
+        try {
+            Statement st = this.getDb().createStatement();
+
+            String query;
+            query = "UPDATE BAKICI " + "\n"
+                    + "SET " + "\n"
+                    + "isim = " + "'" + c.getIsim() + "'" + ",\n"
+                    + "email = " + "'" + c.getEmail() + "'" + ",\n"
+                    + "sifre = " + "'" + c.getSifre() + "'" + "\n"
+                    + "WHERE bakici_id = " + c.getBakici_id() + ";";
+            int r = st.executeUpdate(query);
+
+            System.out.println("DB UPDATE returned with: " + r);
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
@@ -56,20 +75,20 @@ public class BakiciDAO extends DBConnection {
             ResultSet rs = st.executeQuery(query);
 
             while (rs.next()) {
-                bakiciList.add( new Bakici(
+                bakiciList.add(new Bakici(
                         rs.getInt("user_id"),
                         rs.getString("isim"),
-                        rs.getString("email"), 
+                        rs.getString("email"),
                         rs.getString("sifre"),
                         rs.getInt("sinif"),
                         rs.getInt("bakici_id"),
                         rs.getInt("neg_referans"),
                         rs.getInt("poz_referans"),
                         rs.getInt("gecmis_alisveris")
-                    )
+                )
                 );
             }
-            
+
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
