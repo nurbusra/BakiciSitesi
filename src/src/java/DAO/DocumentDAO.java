99724a838ebfabf73118d2278d5f018503162379
mer <->
2022-06-01 14:12:13 +0300

    dosya işlemi yapıldı

---
+++ src/src/java/DAO/DocumentDAO.java
@@ -0,0 +1,18 @@
+
+package DAO;
+
+/**
+ *
+ * @author Merve
+ */
+
+import Entity.Document;
+import java.util.ArrayList;
+import java.util.List;
+public class DocumentDAO {
+    public List<Document> findAll(){
+        List<Document> dList=new ArrayList<>();
+        return dList;
+    }
+    
+}
